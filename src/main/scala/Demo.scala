57b553e43829749218efb866a52e6691614a94a7
ChanakaA <chanaka.anuruddha@pagero.com>
2019-08-07 10:43:32 +0530

    first commit

---
+++ src/main/scala/Demo.scala
@@ -0,0 +1,21 @@
+class TestApply {
+  def apply() {
+    println("inside the apply method")
+  }
+
+  def sum(int: Int) = int + 5
+}
+
+object Demo {
+  def main(args: Array[String]): Unit = {
+    import java.io._
+    val pw = new PrintWriter(new File("hello.txt" ))
+    for( a <- 0 to 20000){
+      pw.write(a+"\n")
+    }
+
+    pw.close
+
+  }
+}
+
